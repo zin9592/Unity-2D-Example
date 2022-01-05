47e3bba9178d2bee812dbd64d220a001bde7984a
zin9592 <young_9592@naver.com>
2022-01-05 18:08:24 +0900

    B18 Last

--- 2D Unity Project1/Assets/Scripts/EnemyMove.cs
+++ 2D Unity Project1/Assets/Scripts/EnemyMove.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
 
     // 행동지표를 결정할 변수 하나 생성
     public int nextMove;
+    public int moveSpeed;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         // 주어진 시간이 지난 뒤 지정된 함수를 실행하는 함수
         Invoke("Think", 3);
@@ -20,7 +25,18 @@ public class EnemyMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(nextMove,rigid.velocity.y);
+        // Move
+        rigid.velocity = new Vector2(nextMove * moveSpeed, rigid.velocity.y);
+
+        // Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + (nextMove * 0.5f), rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, Color.green);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        // 빔을 아래로 쏴서 맞으면 동작
+        if (rayHit.collider == null)
+        {
+            Turn();
+        }
     }
 
     // 몬스터의 행동지표를 바꿔줄 함수
@@ -28,11 +44,32 @@ public class EnemyMove : MonoBehaviour
     void Think()
     {
         // Random.Range() : 최소 ~ 최대 범위의 랜덤 수 생성(최대는 제외됨)
-        // 왼쪽, 정지, 오른쪽 변수
+        // RandomMoveSpeed
         nextMove = Random.Range(-1,2);
+        ChangeAnimation(nextMove);
 
-        // 딜레이 필요 무조건
-        Invoke("Think", 3);
-        //Think();
+        //Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void ChangeAnimation(int nextMove)
+    {
+        // Animation
+        animator.SetInteger("walkSpeed", nextMove);
+        // direction
+        if (nextMove != 0)
+        {
+            spriteRenderer.flipX = nextMove == 1;
+        }
+    }
+
+    void Turn()
+    {
+        nextMove = nextMove * -1;
+        ChangeAnimation(nextMove);
+        // 모든 인보크 정지
+        CancelInvoke();
+        Invoke("Think", 1);
     }
 }
