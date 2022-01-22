d39e9047438d38f7a2ff4f87c6df89328efecc0b
zin9592 <young_9592@naver.com>
2022-01-22 18:34:56 +0900

    2022-01-22_2

--- TopDown_Example/Assets/Script/PlayerAction.cs
+++ TopDown_Example/Assets/Script/PlayerAction.cs
@@ -6,23 +6,64 @@ public class PlayerAction : MonoBehaviour
 {
     float h;
     float v;
+    bool isHorizonMove;
+
     public float moveSpeed;
 
     Rigidbody2D rigid;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        h = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        v = Input.GetAxisRaw("Vertical") * moveSpeed;
+        //Move Value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        //Check Button Down & Up
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        //Check Horizontal Move
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }else if (hUp || vUp)
+        {
+            isHorizonMove = h != 0;
+        }
+
+        //Animation
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * moveSpeed;
     }
 }
