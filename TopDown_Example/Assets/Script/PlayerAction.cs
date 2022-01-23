01de817e04a93fd80f68b20ef1e2ad7bb05791c2
zin9592 <young_9592@naver.com>
2022-01-23 18:49:12 +0900

    2022-01-23

--- TopDown_Example/Assets/Script/PlayerAction.cs
+++ TopDown_Example/Assets/Script/PlayerAction.cs
@@ -12,6 +12,8 @@ public class PlayerAction : MonoBehaviour
 
     Rigidbody2D rigid;
     Animator anim;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     void Awake()
     {
@@ -39,7 +41,8 @@ public class PlayerAction : MonoBehaviour
         else if (vDown)
         {
             isHorizonMove = false;
-        }else if (hUp || vUp)
+        }
+        else if (hUp || vUp)
         {
             isHorizonMove = h != 0;
         }
@@ -59,11 +62,54 @@ public class PlayerAction : MonoBehaviour
         {
             anim.SetBool("isChange", false);
         }
+
+        // Direction
+        if (hDown && h == -1)
+        {
+            //left
+            dirVec = Vector3.left;
+        }
+        else if (hDown && h == 1)
+        {
+            //right
+            dirVec = Vector3.right;
+        }
+        else if (vDown && v == -1)
+        {
+            //down
+            dirVec = Vector3.down;
+        }
+        else if (vDown && v == 1)
+        {
+            //up
+            dirVec = Vector3.up;
+        }
+
+        //Scan Object
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+        {
+            Debug.Log("This is :" + scanObject.name);
+        }
     }
 
     void FixedUpdate()
     {
+        // Character Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rigid.velocity = moveVec * moveSpeed;
+
+        // Ray
+        //Scan Object
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if(rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+        {
+            scanObject = null;
+        }
     }
 }
