65ef55bcb394579b4f733a6a3daf8770e351728c
zin9592 <young_9592@naver.com>
2022-02-04 16:46:29 +0900

    2022-02-04

--- TopDown_Example/Assets/Script/GameManager.cs
+++ TopDown_Example/Assets/Script/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
 
     public GameObject _scanObject;
     public bool _isAction;
-    public GameObject _talkPanel;
     public Image _portraitImg;
+    public Animator _protraitAnimator;
+    public Sprite _prevPortrait;
     public Text _talkText;
+    public Animator _talkPanel;
     public int _talkIndex;
     public TalkManager _talkManager;
     public QuestManager _questManager;
+
 
     void Start()
     {
@@ -28,9 +31,9 @@ public class GameManager : MonoBehaviour
         _scanObject = scanObject;
         ObjectData objectData = _scanObject.GetComponent<ObjectData>();
         Talk(objectData._id, objectData._isNPC);
-        
+
         //Visible Talk for Action
-        _talkPanel.SetActive(_isAction);
+        _talkPanel.SetBool("isShow", _isAction);
     }
 
     void Talk(int id, bool isNPC)
@@ -51,13 +54,24 @@ public class GameManager : MonoBehaviour
         {
             //NPC Talk
             _talkText.text = talkData.Split(':')[0];
+
+            //Show Portrait
             _portraitImg.sprite = _talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
             _portraitImg.color = new Color(1, 1, 1, 1);
+            
+            //Animation Portait
+            if (_prevPortrait != _portraitImg.sprite)
+            {
+                _protraitAnimator.SetTrigger("doEffect");
+                _prevPortrait = _portraitImg.sprite;
+            }
         }
         else
         {
             //Object Talk
             _talkText.text = talkData;
+
+            //Hide Portrait
             _portraitImg.color = new Color(1, 1, 1, 0);
         }
         _isAction = true;
