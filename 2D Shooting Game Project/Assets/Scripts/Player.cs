83d1ac5025572983080e865bed69b96f54e298c5
zin9592 <young_9592@naver.com>
2022-03-16 13:37:46 +0900

    2022-03-16_1

--- 2D Shooting Game Project/Assets/Scripts/Player.cs
+++ 2D Shooting Game Project/Assets/Scripts/Player.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed;
-    public bool isTouchTop;
-    public bool isTouchBottom;
-    public bool isTouchLeft;
-    public bool isTouchRight;
+    public float _moveSpeed;
+    public bool _isTouchTop;
+    public bool _isTouchBottom;
+    public bool _isTouchLeft;
+    public bool _isTouchRight;
+
+    Animator _animator;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();    
+    }
 
     void Update()
     {
         float h = Input.GetAxisRaw("Horizontal");   // 수평
-        if((isTouchRight && h == 1)||(isTouchLeft && h == -1))
+        if((_isTouchRight && h == 1)||(_isTouchLeft && h == -1))
         {
             h = 0;
         }
         float v = Input.GetAxisRaw("Vertical");     // 수직
-        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+        if ((_isTouchTop && v == 1) || (_isTouchBottom && v == -1))
         {
             v = 0;
         }
         Vector3 curPos = transform.position;        // 현재위치
-        Vector3 nextPos = new Vector3(h, v, 0) * moveSpeed * Time.deltaTime;
+        Vector3 nextPos = new Vector3(h, v, 0) * _moveSpeed * Time.deltaTime;
 
         transform.position = curPos + nextPos;
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        {
+            _animator.SetInteger("Input", (int)h);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -35,16 +47,16 @@ public class Player : MonoBehaviour
             switch (collision.gameObject.name)
             {
                 case "Top":
-                    isTouchTop = true;
+                    _isTouchTop = true;
                     break;
                 case "Bottom":
-                    isTouchBottom = true;
+                    _isTouchBottom = true;
                     break;
                 case "Left":
-                    isTouchLeft = true;
+                    _isTouchLeft = true;
                     break;
                 case "Right":
-                    isTouchRight = true;
+                    _isTouchRight = true;
                     break;
             }
         }
@@ -57,16 +69,16 @@ public class Player : MonoBehaviour
             switch (collision.gameObject.name)
             {
                 case "Top":
-                    isTouchTop = false;
+                    _isTouchTop = false;
                     break;
                 case "Bottom":
-                    isTouchBottom = false;
+                    _isTouchBottom = false;
                     break;
                 case "Left":
-                    isTouchLeft = false;
+                    _isTouchLeft = false;
                     break;
                 case "Right":
-                    isTouchRight = false;
+                    _isTouchRight = false;
                     break;
             }
         }
