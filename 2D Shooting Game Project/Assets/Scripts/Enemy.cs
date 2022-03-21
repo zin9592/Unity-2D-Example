73382f125a943d0fc2cf6a189f90d0863c740ed4
zin9592 <young_9592@naver.com>
2022-03-21 18:39:30 +0900

    2022-03-21_1

--- 2D Shooting Game Project/Assets/Scripts/Enemy.cs
+++ 2D Shooting Game Project/Assets/Scripts/Enemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -8,14 +6,63 @@ public class Enemy : MonoBehaviour
     public int _health;
     public Sprite[] _sprites;    // 기본 이미지, 피격 이미지
 
+    // 발사체
+    public float _curShotDelay;         // 한발쏜 다음 충전되기 위한 딜레이
+    public float _maxShotDelay;         // 실제 딜레이
+
+    // 발사체 오브젝트
+    public GameObject _bulletObjA;
+    public GameObject _bulletObjB;
+
+    public GameObject _player;
+
     SpriteRenderer _spriteRenderer;
-    Rigidbody2D _rigid;
 
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _rigid = GetComponent<Rigidbody2D>();
-        _rigid.velocity = Vector2.down * _moveSpeed;
+    }
+
+    void Update()
+    {
+        Fire();
+        Reload();
+    }
+
+    void Fire()
+    {
+        if (_curShotDelay < _maxShotDelay)
+        {
+            return;
+        }
+        if (gameObject.name == "Enemy S(Clone)")
+        {
+            GameObject bullet = Instantiate(_bulletObjA, transform.position, transform.rotation);
+            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+            // 바라보는 각도
+            Vector3 dirVec = _player.transform.position - transform.position;
+            rigid.AddForce(dirVec.normalized * 10, ForceMode2D.Impulse);
+        }
+        else if (gameObject.name == "Enemy L(Clone)")
+        {
+            GameObject bulletL = Instantiate(_bulletObjB, transform.position + Vector3.left * 0.3f, transform.rotation);
+            GameObject bulletR = Instantiate(_bulletObjB, transform.position + Vector3.right * 0.3f, transform.rotation);
+
+            Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+            Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+
+            Vector3 dirVecL = _player.transform.position - transform.position + Vector3.left * 0.3f;
+            Vector3 dirVecR = _player.transform.position - transform.position + Vector3.right * 0.3f;
+
+            rigidL.AddForce(dirVecL.normalized * 10, ForceMode2D.Impulse);
+            rigidR.AddForce(dirVecR.normalized * 10, ForceMode2D.Impulse);
+        }
+        _curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        _curShotDelay += Time.deltaTime;
     }
 
     // 피격
@@ -39,10 +86,10 @@ public class Enemy : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         // 맵 밖으로 나간 일반기체는 삭제
-        if(collision.gameObject.tag == "BorderBullet")
+        if (collision.gameObject.tag == "BorderBullet")
         {
             Destroy(gameObject);
-        }    
+        }
         else if (collision.gameObject.tag == "PlayerBullet")
         {
             Bullet bullet = collision.gameObject.GetComponent<Bullet>();
--- 2D Shooting Game Project/Assets/Scripts/GameManager.cs
+++ 2D Shooting Game Project/Assets/Scripts/GameManager.cs
@@ -5,28 +5,49 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] _enemyObjects;   // 스폰되는 적들의 종류
-    public Transform[] _spawnPoints;     // 5개의 스폰포인트
+    public Transform[] _spawnPoints;     // 적들의 스폰위치
     public float _maxSpawnDelay;
     public float _curSpawnDelay;
+
+    public GameObject _player;
 
     void Update()
     {
         _curSpawnDelay += Time.deltaTime;
-        
-        if(_curSpawnDelay > _maxSpawnDelay)
+
+        if (_curSpawnDelay > _maxSpawnDelay)
         {
             SpawnEnemy();
-            _maxSpawnDelay = Random.Range(0.5f, 5f);
+            _maxSpawnDelay = Random.Range(0.5f, 3f);
             _curSpawnDelay = 0;
         }
     }
 
     void SpawnEnemy()
     {
-        int randomEnemy = Random.Range(0, 3);
-        int randomPoint = Random.Range(0, 5);
-        Instantiate(_enemyObjects[randomEnemy], 
-            _spawnPoints[randomPoint].position, 
-            _spawnPoints[randomPoint].rotation);
+        int randomEnemy = Random.Range(0, _enemyObjects.Length);
+        int randomPoint = Random.Range(0, _spawnPoints.Length);
+        GameObject enemy = Instantiate(_enemyObjects[randomEnemy],
+                            _spawnPoints[randomPoint].position,
+                            _spawnPoints[randomPoint].rotation);
+
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic._player = _player;
+
+        if (randomPoint == 5 || randomPoint == 6)   // Left Spawn
+        {
+            rigid.velocity = new Vector2(enemyLogic._moveSpeed, -1);
+            enemy.transform.Rotate(Vector3.forward * 90);
+        }
+        else if (randomPoint == 7 || randomPoint == 8)  // Right Spawn
+        {
+            rigid.velocity = new Vector2(enemyLogic._moveSpeed * (-1), -1);
+            enemy.transform.Rotate(Vector3.back * 90);
+        }
+        else // Top Spawn
+        {
+            rigid.velocity = new Vector2(0, enemyLogic._moveSpeed * (-1));
+        }
     }
 }
