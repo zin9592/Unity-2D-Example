8f793ff1a0598f56dad53fae898007fd28ee4824
zin9592 <young_9592@naver.com>
2022-04-24 20:47:42 +0900

    2022-04-24_2

--- Quad Action/Assets/Scripts/Player.cs
+++ Quad Action/Assets/Scripts/Player.cs
@@ -11,8 +11,10 @@ public class Player : MonoBehaviour
     bool _wDown;
     bool _jDown;
     bool _isJump;
+    bool _isDodge;
 
     Vector3 _moveVector;
+    Vector3 _dodgeVector;
     Animator _animator;
     Rigidbody _rigidbody;
 
@@ -29,6 +31,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
 
     void GetInput()
@@ -42,8 +45,16 @@ public class Player : MonoBehaviour
 
     void Move()
     {
-        // Player Move
+        // Player moveVector
         _moveVector = new Vector3(_hAxis, 0, _vAxis).normalized;
+
+        // isDodge?
+        if (_isDodge)
+        {
+            _moveVector = _dodgeVector;
+        }
+
+        // Move
         transform.position += _moveVector * _speed * (_wDown == true ? 0.3f : 1) * Time.deltaTime;
 
         // Animator Trigger
@@ -57,13 +68,15 @@ public class Player : MonoBehaviour
         transform.LookAt(transform.position + _moveVector);
     }
 
+    // 조건
+    // 제자리에서 점프키를 누를 시 점프
     void Jump()
     {
-        if (_jDown && !_isJump)
+        if (_jDown && _moveVector == Vector3.zero && !_isJump && !_isDodge)
         {
             // Jump Force
             _rigidbody.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
-            
+
             // Animator Trigger 
             _animator.SetBool("isJump", true);
             _animator.SetTrigger("doJump");
@@ -73,9 +86,29 @@ public class Player : MonoBehaviour
         }
     }
 
+    // 조건
+    // 방향키 + 점프키를 누를 시 회피
+    void Dodge()
+    {
+        if (_jDown && _moveVector != Vector3.zero && !_isJump && !_isDodge)
+        {
+            _dodgeVector = _moveVector;
+            _speed *= 2;
+            _animator.SetTrigger("doDodge");
+            _isDodge = true;
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        _speed *= 0.5f;
+        _isDodge = false;
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Floor")
+        if (collision.gameObject.tag == "Floor")
         {
             // Animator Trigger 
             _animator.SetBool("isJump", false);
